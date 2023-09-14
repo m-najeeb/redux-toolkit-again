89e01f47651f7920f3dc823ea0f8b887fd912206
emi-najeeb <muhammadnajeeb@emifusion.com>
2023-09-14 11:40:09 +0500

    configureStore imported

---
+++ src/store/store.js
@@ -0,0 +1,7 @@
+//create a folder named store and add a file store.js
+//import configureStore from @reduxjs/toolkit
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {},
+});
